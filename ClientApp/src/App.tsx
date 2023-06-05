382739d13e9d48fe4f928a8a9b40de236ae6a901
Lawrence Thatcher <lwthatcher@msn.com>
2023-06-05 12:51:09 -0600

    wrapping app in date-time localization context

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -9,6 +9,10 @@ import { FeaturesLoader } from './contexts/features';
 import CustomTheme from './CustomTheme';
 import AppRouting from './AppRouting';
 
+// imports for date/time localization
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 const App: FC = () => {
   const notistackRef: RefObject<any> = React.createRef();
 
@@ -18,22 +22,24 @@ const App: FC = () => {
   // console.log("URL: %s", window.location.href);
 
   return (
-    <CustomTheme>
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        ref={notistackRef}
-        action={(key) => (
-          <IconButton onClick={onClickDismiss(key)} size="small">
-            <CloseIcon />
-          </IconButton>
-        )}
-      >
-        <FeaturesLoader>
-          <AppRouting />
-        </FeaturesLoader>
-      </SnackbarProvider>
-    </CustomTheme>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <CustomTheme>
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          ref={notistackRef}
+          action={(key) => (
+            <IconButton onClick={onClickDismiss(key)} size="small">
+              <CloseIcon />
+            </IconButton>
+          )}
+        >
+          <FeaturesLoader>
+            <AppRouting />
+          </FeaturesLoader>
+        </SnackbarProvider>
+      </CustomTheme>
+    </LocalizationProvider>
   );
 };
 
